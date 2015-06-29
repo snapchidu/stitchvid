ba212161c6f7659462e90e28e4cfad9c63cbe8fc
chidumaga <chidumaga@gmail.com>
2015-06-29 23:51:48 +0100

    loads video from db based on uid

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,12 @@
 class VideosController < ApplicationController
 
   def index
+    @videos = Video.all
+    @uids = []
+
+    @videos.each do |video|
+      @uids.push(video.uid)
+    end
 
   end
 
