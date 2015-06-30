72e26abaa67f8ffe40be8df6dd84da1fc7a7270e
chidumaga <chidumaga@gmail.com>
2015-06-30 12:01:17 +0100

    adds tests

--- config/initializers/yt.rb
+++ config/initializers/yt.rb
@@ -1,4 +1,4 @@
 Yt.configure do |config|
- config.api_key = ' <%= ENV[YT_SERVER_KEY] %> '
+ config.api_key = ENV['YT_SERVER_KEY']
  config.log_level = :debug
 end
--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -7,7 +7,6 @@ feature 'Videos' do
     expect(page).to have_link('Add Story')
   end
 
-
   #test shows videos
   scenario 'shows videos on homepage' do
     Video.create(link: "https://www.youtube.com/watch?t=32&v=3Fhx2YgbskE")
@@ -15,8 +14,15 @@ feature 'Videos' do
     expect(page).to have_css('iframe')
   end
 
-
   #test upload then shows homepage
+  scenario 'uploads videos' do
+    visit '/'
+    click_link 'Add Story'
+    fill_in("Link", with: "https://www.youtube.com/watch?t=32&v=3Fhx2YgbskE")
+    click_button 'Create Video'
 
+    expect(current_path).to eq('/')
+    expect(page).to have_css('iframe')
+  end
 
 end
