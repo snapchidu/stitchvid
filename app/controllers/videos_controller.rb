b72599236fdc1f472fc3be1f49ffdb51927462fe
Alex Handy <handy437@gmail.com>
2015-06-30 10:19:01 +0100

    Completed basic multiple videos on page programmatically

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,12 +2,6 @@ class VideosController < ApplicationController
 
   def index
     @videos = Video.all
-    @uids = []
-
-    @videos.each do |video|
-      @uids.push(video.uid)
-    end
-
   end
 
 end
