13cd1afcb1926344bc5dff5b8907ceea15d8b57c
DanielJohnston <macademy@danieljohnston.co.uk>
2015-06-30 12:03:08 +0100

    videos controller and view now generate playlist

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,6 +2,8 @@ class VideosController < ApplicationController
 
   def index
     @videos = Video.all
+    #The playlist creation is completely arbitrary, just generates something we can use
+    @playlist = Video.offset(1)
   end
 
 end
