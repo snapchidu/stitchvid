a23736cf48a4ca6e8787390aa548221a250f8bd3
Alex Handy <handy437@gmail.com>
2015-07-01 21:38:23 +0100

    Working post upload request from iPhone app

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,8 +1,14 @@
 class VideosController < ApplicationController
-
+  respond_to :html, :json
   def index
     @tags = Tag.all
-    
+
+    @videos = Video.all
+    respond_with(@videos.to_json)
+    # respond_to do |format|
+    #   @videos = Video.all
+    #   format.json { render :json => @videos.to_json}
+    # end
   end
 
   def new
@@ -10,8 +16,11 @@ class VideosController < ApplicationController
   end
 
   def create
+    @test = params[:link]
+    puts @test
 
-    @video =  Video.create(params.require(:video).permit(:link, :all_tags))
+    @video = Video.create(link: params[:link], all_tags: params[:all_tags])
+    # @video =  Video.create(params.require(:video).permit(:link, :all_tags))
     if @video.save
       redirect_to '/'
     else
