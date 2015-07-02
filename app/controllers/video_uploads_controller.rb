c696b78d01e40f4761d41044950084528df5e6ec
Alex Handy <handy437@gmail.com>
2015-07-02 16:11:38 +0100

    Added Tag Form to Uploader

--- app/controllers/video_uploads_controller.rb
+++ app/controllers/video_uploads_controller.rb
@@ -7,13 +7,16 @@ class VideoUploadsController < ApplicationController
     @video_upload = VideoUpload.new(title: params[:video_upload][:title],
                                   description: params[:video_upload][:description],
                                   file: params[:video_upload][:file].try(:tempfile).try(:to_path))
+
+    @tag = params[:all_tags]
+
     if @video_upload.save
       uploaded_video = @video_upload.upload!(current_user)
 
       if uploaded_video.failed?
         flash[:error] = 'There was an error while uploading your video...'
       else
-        Video.create({link: "https://www.youtube.com/watch?v=#{uploaded_video.id}", all_tags: 'test'})
+        Video.create({link: "https://www.youtube.com/watch?v=#{uploaded_video.id}", all_tags: @tag})
         flash[:success] = 'Your video has been uploaded!'
       end
 
