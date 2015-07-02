d4b2368ec1cf2931de2882b80fb1b451664b7659
chidumaga <chidumaga@gmail.com>
2015-07-02 20:10:08 +0100

    runs on localhost

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-provider :google_oauth2, ENV['YT_CLIENT_ID_TWO'], ENV['YT_CLIENT_SECRET_TWO'], scope: 'userinfo.profile,youtube'
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
+provider :google_oauth2, ENV['YT_CLIENT_ID'], ENV['YT_CLIENT_SECRET'], scope: 'userinfo.profile,youtube'
 end
