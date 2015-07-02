9f7fff6613a9bb1a7a6b0656213d11fbcea7a793
Alex Handy <handy437@gmail.com>
2015-07-02 14:55:10 +0100

    Prepared for Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # before_action :initialize_omniauth_state
-
-  # protected
-
-  # def initialize_omniauth_state
-  #   session['omniauth.state'] = response.headers['X-CSRF-Token'] = form_authenticity_token
-  # end
-
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
