e6dfcb2f538147793a5d9fddaeaa21d698612dcd
DanielJohnston <macademy@danieljohnston.co.uk>
2015-07-03 17:24:26 +0100

    added error handling for missing YouTube token and install instructions

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,8 +2,11 @@ class VideosController < ApplicationController
 
   def index
     Video.unpublished.each do |video|
+      begin
       video_status = HTTParty.get "https://www.googleapis.com/youtube/v3/videos?part=status&id=#{video.uid}&fields=items%2Fstatus&key=#{Rails.application.secrets.youtube_token}"
       video.update_columns(processed: true) if video_status["items"][0]["status"]["uploadStatus"] == "processed"
+      rescue
+      end
     end
     @tags = Tag.top(5)
   end
