b1282dea2b81e6d9edc53eb19dc839ba0e18a4ec
Alex Handy <handy437@gmail.com>
2015-07-08 10:56:22 +0100

    Reset credentials so works on local and heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
-provider :google_oauth2, ENV['YT_CLIENT_ID_TWO'], ENV['YT_CLIENT_SECRET_TWO'], scope: 'userinfo.profile,youtube'
+provider :google_oauth2, ENV['YT_CLIENT_ID'], ENV['YT_CLIENT_SECRET'], scope: 'userinfo.profile,youtube'
 end
